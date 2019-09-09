3fce58b1cf7f18bdab10c8f6b5519c61a2ad7cc3
Andres Alba <bryanalba92@gmail.com>
2019-09-09 01:17:53 -0500

    Probando números

---
+++ src/app/basicas/numeros/numeros.spec.ts
@@ -0,0 +1,12 @@
+import { incrementar } from './numeros';
+describe( 'Pruebas de numero', () => {
+    it( 'debe de retornar 100 si el numero ingresado es mayor a 100', () => {
+       const resp = incrementar(300);
+       expect( resp ).toBe(100);
+    } );
+
+    it( 'debe de retornar el numero ingresado + 1, si no es mayor a 100', () => {
+        const resp = incrementar(50);
+        expect( resp ).toBe(51);
+     } );
+});
