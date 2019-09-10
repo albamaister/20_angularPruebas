c802e6a1328df3f1126808777f7259002674935a
Andres Alba <bryanalba92@gmail.com>
2019-09-10 16:05:04 -0500

    Realizar pruebas en el HTML del componente

--- src/app/intermedio2/incrementador/incrementador.component.spec.ts
+++ src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { IncrementadorComponent } from './incrementador.component';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 
 describe('Incremendator Component', () => {
@@ -19,8 +20,15 @@ describe('Incremendator Component', () => {
 
     });
 
-    it('', () => {
+    it('Debe de mostar la leyenda', () => {
 
+        component.leyenda = 'Progreso de carga';
+
+        fixture.detectChanges(); // disparar la deteccion de cambios
+        const elem: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
+        // Obteniendo el elemento html del fixure que es el elemento compilado de angular
+
+        expect( elem.innerHTML ).toContain('Progreso de carga');
 
     });
 
