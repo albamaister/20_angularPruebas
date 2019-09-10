c77b1348477a3bc98f15691d7a4ebc9504ce25cb
Andres Alba <bryanalba92@gmail.com>
2019-09-10 15:02:54 -0500

    Servicios en el TestingModule-Uso de servicios

--- src/app/intermedio2/medico/medico.component.spec.ts
+++ src/app/intermedio2/medico/medico.component.spec.ts
@@ -3,6 +3,8 @@ import { TestBed, ComponentFixture } from '@angular/core/testing';
 // ComponentFixture nos va a permitir controlar o tener acceso a todo el html
 
 import { MedicoComponent } from './medico.component';
+import { MedicoService } from './medico.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 describe('Medico componet', () => {
@@ -11,13 +13,27 @@ describe('Medico componet', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [ MedicoComponent ]
+            declarations: [ MedicoComponent ],
+            providers: [ MedicoService ],
+            imports: [HttpClientModule]
         });
         fixture = TestBed.createComponent(MedicoComponent);
         component = fixture.componentInstance;
     });
 
-    it( '', () => {
+    it( 'Debe de crearse el componente', () => {
+
+        expect(component).toBeTruthy();
+
+    });
+
+
+    it( 'Debe de retornar el nombre del medico', () => {
+
+        const nombre = 'Juan';
+        const res = component.saludarMedico( nombre );
+
+        expect(res).toContain(nombre);
 
     });
 });
